3b8854b06162cf225d30c9c325200104a49fae59
Ko4lc0 <Sega.X2012@yandex.ru>
2024-08-09 15:46:40 +0300

    initial commit

---
+++ Homework8/homework8.py
@@ -0,0 +1,11 @@
+numbers = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+index = 0
+while index < len(numbers):
+    if numbers[index] > 0:
+        print(numbers[index])
+        index += 1
+    else:
+        index += 1
+        continue
+
+
