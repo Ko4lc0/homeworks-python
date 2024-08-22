dc4e30b3b76a37102695681a081338dd382d9177
Ko4lc0 <Sega.X2012@yandex.ru>
2024-08-22 15:46:38 +0300

    made module_2_5.py commit

--- module_2_2/Module_2_2.py
+++ module_2_2/Module_2_2.py
@@ -3,7 +3,7 @@ second = int(input("введите второе число "))
 third = int(input("введите третье число "))
 if first == second and second == third:
     print(3)
-elif first == second and first != third or first == third and first != second:
-    print(2)
-else:
+elif first != second and first != third and second != third:
     print(0)
+else:
+    print(2)
