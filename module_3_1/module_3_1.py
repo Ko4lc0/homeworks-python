7de249bf62cf33c1bfbde9653c7967c29f5970b6
Ko4lc0 <Sega.X2012@yandex.ru>
2024-09-01 14:49:43 +0300

    change module_3_1.py commit

--- module_3_1/module_3_1.py
+++ module_3_1/module_3_1.py
@@ -8,23 +8,21 @@ def count_calls():
 
 def string_info(string):
     count_calls()
-    string_list = list()
-    string_list.append(len(string))
-    string_list.append(string.lower())
-    string_list.append(string.upper())
-    return tuple(string_list)
+    return len(string), string.lower(), string.upper()
 
 
 def is_contains(string, input_list):
     count_calls()
 
-    if string in input_list:
-        return True
-    else:
-        return False
+    for word in input_list:
+        if word.lower() == string.lower():
+            return True
+
+    return False
+
 
 print(string_info('Capybara'))
 print(string_info('Armageddon'))
-print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
-print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+print(is_contains('cycle', ['recycling', 'cyclic']))
 print(calls)
